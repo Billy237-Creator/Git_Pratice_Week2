3c256ba35613bfdf111056f4d6af455bfea0fa61
Billy <kombo.bertrand@student.nye.hu>
2026-09-21 21:48:33 +0200

    Add Feature2_branch

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is Feature_branch");
+        System.out.println("This is Feature2_branch");
     }
 }
